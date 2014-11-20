4ae63e49f92f73d3212c94bc27c7fdca0cfd4181
mherediaf <martin.herediaf@gmail.com>
2014-11-20 16:33:27 -0300

    mas tokens (para aritmetica), sus definiciones en regex. Mas producciones de la gramatica (para aritmetica y para precedencia de operadores AND y OR de los elementos)

--- tokens.py
+++ tokens.py
@@ -1,31 +1,52 @@
+import ply.lex as lex
+
 tokens=(
-	'NUM', 'RULE', 'BALL', 'BOX', 'VOID', 'ROTATION', 'SCALE', 'COLOR', 'TRANSLATION', 'DEPTH', 'EQUALS', 'DDOTE', 'X', 'Y', 'Z', 'R', 'G', 'B', 'AND',
-	'OR', 'POT', 'LBRACKET', 'RBRACKET', 'LESS', 'GREATER', 'POINT'
+	'NUM', 'RULE', 'BALL', 'BOX', 'VOID', 'ROTATION', 'SCALE', 'COLOR', 'TRANSLATION', 'DEPTH', 'EQUALS', 'DDOTE', 'X', 'Y', 'Z', 'R', 'G', 'B', 'AND', 'ADD', 'SUM', 'MULT', 'DIV', 'OR', 'POT', 'LBRACKET', 'RBRACKET', 'LESS', 'GREATER', 'POINT'
 	)
 
 t_RULE = r'[a-ZA-Z]+|\x24'
+
 t_BALL=r'ball'
 t_BOX=r'box'
+
 t_VOID=r'_'
 t_ROTATION=r'r'
 t_SCALE=r's'
 t_TRANSLATION=r't'
 t_COLOR=r'c'
 t_DEPTH=r'd'
+
 t_EQUALS=r'='
 t_DDOTE=r':'
+
 t_X=r'x'
 t_Y=r'y'
 t_Z=r'z'
 t_R=r'r'
 t_G=r'g'
 t_B=r'b'
+
 t_AND=r'&'
 t_OR=r'\x7C'
+
+t_ADD=r'\+'
+t_SUB=r'-'
+t_MUL=r'\*'
+t_DIV=r'/'
 t_POT=r'\x5E'
+
 t_LBRACKET=r'\x5B'
 t_RBRACKET=r'\x5D'
+
 t_LESS=r'\x3C'
 t_GREATER=r'\x3E'
+
 t_POINT=r'\x2E'
 
+def t_NUM(t):
+	r'\d+(\.\d+)?'
+	t.value = float(t.value)
+	return t
+
+lexer = lex.lex()
+print(lexer)
