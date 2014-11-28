12f9befc40125479b057ca5658541f3b9a88ef57
mherediaf <martin.herediaf@gmail.com>
2014-11-27 23:48:51 -0300

    archivo principal con nombre copado

---
+++ grapher.py
@@ -0,0 +1,15 @@
+from yacc import parser
+import sys
+
+from lexer import lexer
+
+from definition import *
+from transformation import *
+
+ejemplo = open(sys.argv[1])
+s = ejemplo.read()	
+result = parser.parse(s)
+
+#printing result
+if ("$" in rule2definition_Dict):
+	rule2definition_Dict['$'].show(Transformation())
