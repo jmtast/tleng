272d80e0c74a5973c11e1979b029ec4fef3218db
Pablo González Alba <pablo.gonzalez.alba@gmail.com>
2015-04-19 20:06:54 -0300

    Print to either stdout or file

--- tp1/src/non_deterministic_finite_automata.py
+++ tp1/src/non_deterministic_finite_automata.py
@@ -1,7 +1,8 @@
 from finite_automata import *
+import sys
 
-def print_line(elements):
-    print '\t'.join(map(lambda x: str(x), elements))
+def print_line(elements, file = sys.stdout):
+    file.write('\t'.join(map(lambda x: str(x), elements)) + '\n')
 
 class Transition:
 
@@ -10,8 +11,8 @@ class Transition:
         self.src = src
         self.dst = dst
 
-    def print_transition(self):
-        print_line([self.src, self.label, self.dst])
+    def print_transition(self, file = sys.stdout):
+        print_line([self.src, self.label, self.dst], file)
 
 class NonDeterministicFiniteAutomata(FiniteAutomata):
 
@@ -22,13 +23,13 @@ class NonDeterministicFiniteAutomata(FiniteAutomata):
         self.q0 = q0
         self.final_states = final_states
 
-    def print_automata(self):
-        print_line(self.states)
-        print_line(self.alphabet)
-        print(self.q0)
-        print_line(self.final_states)
+    def print_automata(self, file = sys.stdout):
+        print_line(self.states, file)
+        print_line(self.alphabet, file)
+        file.write(str(self.q0) + '\n')
+        print_line(self.final_states, file)
         for transition in self.transitions:
-            transition.print_transition()
+            transition.print_transition(file)
 
     def add_transition(self, label, src, dst):
         self.transitions.append(Transition(label, src, dst))
