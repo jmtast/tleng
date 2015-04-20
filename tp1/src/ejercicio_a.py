2acbccfac75c0e6eda58acb502eada1b39db909a
Pablo González Alba <pablo.gonzalez.alba@gmail.com>
2015-04-19 21:22:26 -0300

    Read regexp with chars and PLUS

--- tp1/src/ejercicio_a.py
+++ tp1/src/ejercicio_a.py
@@ -2,7 +2,6 @@
 #!/usr/bin/python
 from non_deterministic_finite_automata import *
 
-
 def afd_minimo(archivo_regex, archivo_automata):
 	# file to Regex
 	# Regex to AFND
@@ -12,4 +11,5 @@ def afd_minimo(archivo_regex, archivo_automata):
 	afd = NonDeterministicFiniteAutomata([0, 1, 2], ['a', 'b'])
 	afd.add_transition('a', 0, 1)
 	afd.add_transition('b', 1, 2)
-	afd.print_automata()
+	#afd.print_automata()
+	NonDeterministicFiniteAutomata.from_file(archivo_regex).print_automata()
--- tp1/src/non_deterministic_finite_automata.py
+++ tp1/src/non_deterministic_finite_automata.py
@@ -1,6 +1,8 @@
 from finite_automata import *
 import sys
 
+LAMBDA = 'lambda'
+
 def print_line(elements, file = sys.stdout):
     file.write('\t'.join(map(lambda x: str(x), elements)) + '\n')
 
@@ -15,6 +17,37 @@ class Transition:
         print_line([self.src, self.label, self.dst], file)
 
 class NonDeterministicFiniteAutomata(FiniteAutomata):
+    @classmethod
+    def from_file(cls, file):
+        return cls.__from_lines(file.readlines())
+
+    @classmethod
+    def __from_lines(cls, lines):
+        if '{PLUS}' in lines[0]:
+            automata = cls.__from_lines(lines[1:])
+
+            new_q0 = automata.max_state() + 1
+            automata.add_state(new_q0)
+            
+            new_qf = automata.max_state() + 1
+            automata.add_state(new_qf)
+
+            automata.add_transition(LAMBDA, new_q0, automata.q0)
+            
+            for qf in automata.final_states:
+                automata.add_transition(LAMBDA, qf, automata.q0)
+                automata.add_transition(LAMBDA, qf, new_qf)
+
+            automata.final_states = [new_qf]
+            automata.q0 = new_q0
+        else:
+            char = lines[0].strip()
+            automata = NonDeterministicFiniteAutomata([0, 1], [char], [], 0, [1])
+            automata.add_transition(char, 0, 1)
+
+        return automata
+
+
 
     def __init__(self, states = [], alphabet = [], transitions = [], q0 = 0, final_states = [0]):
         self.states = states
@@ -27,6 +60,9 @@ class NonDeterministicFiniteAutomata(FiniteAutomata):
         
         self.final_states = final_states
 
+    def max_state(self):
+        return max(self.states)
+
     def print_automata(self, file = sys.stdout):
         print_line(self.states, file)
         print_line(self.alphabet, file)
@@ -36,7 +72,7 @@ class NonDeterministicFiniteAutomata(FiniteAutomata):
             transition.print_transition(file)
 
     def add_transition(self, label, src, dst):
-        if not label in self.alphabet:
+        if label != LAMBDA and not label in self.alphabet:
             raise ValueError('El caracter %s no pertenece al alfabeto' % label)
 
         if not src in self.states:
@@ -47,7 +83,7 @@ class NonDeterministicFiniteAutomata(FiniteAutomata):
 
         self.transitions.append(Transition(label, src, dst))
 
-    def add_state(state):
+    def add_state(self, state):
         if state in self.states:
             raise ValueError('El estado %d ya pertenece al automata' % state)
         self.states.append(state)
